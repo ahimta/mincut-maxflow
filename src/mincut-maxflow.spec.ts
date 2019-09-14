b911bc0b3fcd69d6b2a43797a8afaa29a278e811
Abdullah Alansari <ahimta@gmail.com>
2019-09-14 04:18:13 +0300

    chore: fix tests not complete because of previous bugs:sweat_smile:

--- src/mincut-maxflow.spec.ts
+++ src/mincut-maxflow.spec.ts
@@ -10,34 +10,32 @@ interface IExample {
   readonly mincutMaxflow: IMincutMaxflow
 }
 
-// const INFINITY = Number.MAX_SAFE_INTEGER
-
 const examples: ReadonlyArray<IExample> = [
   {
     graph: {
       nodes: ['s', '1', '2', '3', '4', '5', '6', 't'],
       edges: [
-        { from: 's', to: '1', capacity: 10, flow: 10 },
-        { from: 's', to: '2', capacity: 5, flow: 5 },
-        { from: 's', to: '3', capacity: 15, flow: 10 },
+        { from: 's', to: '1', capacity: 10 },
+        { from: 's', to: '2', capacity: 5 },
+        { from: 's', to: '3', capacity: 15 },
 
-        { from: '1', to: '2', capacity: 4, flow: 0 },
-        { from: '1', to: '4', capacity: 9, flow: 5 },
-        { from: '1', to: '5', capacity: 15, flow: 5 },
+        { from: '1', to: '2', capacity: 4 },
+        { from: '1', to: '4', capacity: 9 },
+        { from: '1', to: '5', capacity: 15 },
 
-        { from: '2', to: '3', capacity: 4, flow: 0 },
-        { from: '2', to: '5', capacity: 8, flow: 5 },
+        { from: '2', to: '3', capacity: 4 },
+        { from: '2', to: '5', capacity: 8 },
 
-        { from: '3', to: '6', capacity: 16, flow: 10 },
+        { from: '3', to: '6', capacity: 16 },
 
-        { from: '4', to: '5', capacity: 15, flow: 0 },
-        { from: '4', to: 't', capacity: 10, flow: 5 },
+        { from: '4', to: '5', capacity: 15 },
+        { from: '4', to: 't', capacity: 10 },
 
-        { from: '5', to: '6', capacity: 15, flow: 0 },
-        { from: '5', to: 't', capacity: 10, flow: 10 },
+        { from: '5', to: '6', capacity: 15 },
+        { from: '5', to: 't', capacity: 10 },
 
-        { from: '6', to: '2', capacity: 6, flow: 0 },
-        { from: '6', to: 't', capacity: 10, flow: 10 }
+        { from: '6', to: '2', capacity: 6 },
+        { from: '6', to: 't', capacity: 10 }
       ]
     },
     source: 's',
@@ -45,126 +43,101 @@ const examples: ReadonlyArray<IExample> = [
 
     mincutMaxflow: {
       // @hack: set elements are order in a way that makes tests pass.
-      mincut: new Set(['3', 's', '6', '2']),
+      mincut: new Set(['s', '3', '6', '2']),
       maxflow: 28
     }
   },
   // @note: possibly non-solvable
-  // {
-  //   graph: {
-  //     nodes: ['s', '1', '2', '3', '4', 't'],
-  //     edges: [
-  //       { from: 's', to: '1', capacity: 10, flow: 9 },
-  //       { from: 's', to: '2', capacity: 5, flow: 4 },
+  {
+    graph: {
+      nodes: ['s', '1', '2', '3', '4', 't'],
+      edges: [
+        { from: 's', to: '1', capacity: 10 },
+        { from: 's', to: '2', capacity: 5 },
 
-  //       { from: '1', to: '3', capacity: 9, flow: 9 },
-  //       { from: '1', to: '4', capacity: 4, flow: 4 },
+        { from: '1', to: '3', capacity: 9 },
+        { from: '1', to: '4', capacity: 4 },
 
-  //       { from: '2', to: '1', capacity: 4, flow: 4 },
-  //       { from: '2', to: '4', capacity: 8, flow: 0 },
+        { from: '2', to: '1', capacity: 4 },
+        { from: '2', to: '4', capacity: 8 },
 
-  //       { from: '3', to: '4', capacity: 15, flow: 0 },
-  //       { from: '3', to: 't', capacity: 10, flow: 9 },
+        { from: '3', to: '4', capacity: 15 },
+        { from: '3', to: 't', capacity: 10 },
 
-  //       { from: '4', to: 't', capacity: 10, flow: 4 }
-  //     ]
-  //   },
-  //   source: 's',
-  //   sink: 't',
+        { from: '4', to: 't', capacity: 10 }
+      ]
+    },
+    source: 's',
+    sink: 't',
 
-  //   mincutMaxflow: {
-  //     mincut: new Set(),
-  //     maxflow: 9999
-  //   }
-  // },
+    mincutMaxflow: {
+      mincut: new Set(['s']),
+      maxflow: 15
+    }
+  },
   // @note: possibly non-solvable
-  // {
-  //   graph: {
-  //     nodes: [
-  //       's',
+  {
+    graph: {
+      nodes: [
+        's',
 
-  //       'alice',
-  //       'bob',
-  //       'carol',
-  //       'dave',
-  //       'eliza',
-  //       'adobe',
-  //       'amazon',
-  //       'facebook',
-  //       'google',
-  //       'yahoo',
+        'alice',
+        'bob',
+        'carol',
+        'dave',
+        'eliza',
 
-  //       't'
-  //     ],
-  //     edges: [
-  //       { from: 's', to: 'alice', capacity: 1, flow: 0 },
-  //       { from: 's', to: 'bob', capacity: 1, flow: 0 },
-  //       { from: 's', to: 'carol', capacity: 1, flow: 0 },
-  //       { from: 's', to: 'dave', capacity: 1, flow: 0 },
-  //       { from: 's', to: 'eliza', capacity: 1, flow: 0 },
+        'adobe',
+        'amazon',
+        'facebook',
+        'google',
+        'yahoo',
 
-  //       { from: 'alice', to: 'adobe', capacity: INFINITY, flow: 0 },
-  //       { from: 'alice', to: 'amazon', capacity: INFINITY, flow: 0 },
-  //       { from: 'alice', to: 'google', capacity: INFINITY, flow: 0 },
+        't'
+      ],
+      edges: [
+        { from: 's', to: 'alice', capacity: 1 },
+        { from: 's', to: 'bob', capacity: 1 },
+        { from: 's', to: 'carol', capacity: 1 },
+        { from: 's', to: 'dave', capacity: 1 },
+        { from: 's', to: 'eliza', capacity: 1 },
 
-  //       { from: 'bob', to: 'adobe', capacity: INFINITY, flow: 0 },
-  //       { from: 'bob', to: 'amazon', capacity: INFINITY, flow: 0 },
+        { from: 'alice', to: 'adobe', capacity: Number.MAX_SAFE_INTEGER },
+        { from: 'alice', to: 'amazon', capacity: Number.MAX_SAFE_INTEGER },
+        { from: 'alice', to: 'google', capacity: Number.MAX_SAFE_INTEGER },
 
-  //       { from: 'carol', to: 'adobe', capacity: INFINITY, flow: 0 },
-  //       { from: 'carol', to: 'facebook', capacity: INFINITY, flow: 0 },
-  //       { from: 'carol', to: 'google', capacity: INFINITY, flow: 0 },
+        { from: 'bob', to: 'adobe', capacity: Number.MAX_SAFE_INTEGER },
+        { from: 'bob', to: 'amazon', capacity: Number.MAX_SAFE_INTEGER },
 
-  //       { from: 'dave', to: 'amazon', capacity: INFINITY, flow: 0 },
-  //       { from: 'dave', to: 'yahoo', capacity: INFINITY, flow: 0 },
+        { from: 'carol', to: 'adobe', capacity: Number.MAX_SAFE_INTEGER },
+        { from: 'carol', to: 'facebook', capacity: Number.MAX_SAFE_INTEGER },
+        { from: 'carol', to: 'google', capacity: Number.MAX_SAFE_INTEGER },
 
-  //       { from: 'eliza', to: 'amazon', capacity: INFINITY, flow: 0 },
-  //       { from: 'eliza', to: 'yahoo', capacity: INFINITY, flow: 0 },
+        { from: 'dave', to: 'amazon', capacity: Number.MAX_SAFE_INTEGER },
+        { from: 'dave', to: 'yahoo', capacity: Number.MAX_SAFE_INTEGER },
 
-  //       { from: 'adobe', to: 't', capacity: 1, flow: 0 },
-  //       { from: 'amazon', to: 't', capacity: 1, flow: 0 },
-  //       { from: 'facebook', to: 't', capacity: 1, flow: 0 },
-  //       { from: 'google', to: 't', capacity: 1, flow: 0 },
-  //       { from: 'yahoo', to: 't', capacity: 1, flow: 0 },
-  //     ]
-  //   },
-  //   source: 's',
-  //   sink: 't',
+        { from: 'eliza', to: 'amazon', capacity: Number.MAX_SAFE_INTEGER },
+        { from: 'eliza', to: 'yahoo', capacity: Number.MAX_SAFE_INTEGER },
 
-  //   mincutMaxflow: {
-  //     mincut: new Set(),
-  //     maxflow: 9999
-  //   }
-  // }
+        { from: 'adobe', to: 't', capacity: 1 },
+        { from: 'amazon', to: 't', capacity: 1 },
+        { from: 'facebook', to: 't', capacity: 1 },
+        { from: 'google', to: 't', capacity: 1 },
+        { from: 'yahoo', to: 't', capacity: 1 },
+      ]
+    },
+    source: 's',
+    sink: 't',
+
+    mincutMaxflow: {
+      mincut: new Set(['s']),
+      maxflow: 5
+    }
+  }
 ]
 
-const derivedExamples = flatMap(examples, ({ graph, source, sink, mincutMaxflow: mm }) => {
-  return [
-    { graph, source, sink, mincutMaxflow: mm },
-    {
-      mincutMaxflow: mm,
-
-      source,
-      sink,
-      graph: {
-        nodes: graph.nodes,
-        edges: graph.edges.map(({ from, to, capacity }) => ({
-          from,
-          to,
-
-          capacity,
-          flow: 0
-        }))
-      }
-    }
-  ]
-})
-
-derivedExamples.forEach(({ graph, source, sink, mincutMaxflow: mm }, i) => {
+examples.forEach(({ graph, source, sink, mincutMaxflow: mm }, i) => {
   test(`${i}`, t => {
     t.deepEqual(mincutMaxflow(graph, source, sink), mm)
   })
 })
-
-function flatMap (xs, mapper) {
-  return Array.prototype.concat.apply([], xs.map(mapper))
-}
